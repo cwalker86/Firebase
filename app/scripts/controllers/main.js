a4c45bbb1be5371142c32f317364d43c2c0daa6f
cwalker86 <rhettwalker86@gmail.com>
2014-11-20 00:01:39 -0500

    Added real time reading with Firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,31 @@
  * Controller of the firebaseApp
  */
 angular.module('firebaseApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout) {
     var rootRef = new Firebase('https://torid-inferno-4604.firebaseio.com/');
     var childRef = rootRef.child('message');
 
+    childRef.on('value', function(snapshot) {
+      $timeout(function () {
+      var snapshotVal = snapshot.val();
+      console.log(snapshotVal);
+      $scope.message = snapshotVal;
+      });
+    });
+
+    $scope.$watch('message.text', function(newVal) {
+      if (!newVal) {
+        return;
+      }
+      childRef.update({
+        text: newVal
+      });
+    });
 
     $scope.setMessage = function() {
       rootRef.child('message').set({
         user: 'Cyrus',
-        text: 'lorem'
+        text: 'Lorem up......'
       });
     };
 
